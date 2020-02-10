82dd9f3b2efddcc993e6bc80811edd3ecdc58d94
ashwinkalyanpur <akalyanpur1974@gmail.com>
2020-02-10 12:10:52 +0530

    ample :#

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import 'typeface-roboto';
-import logo from './logo.ico';
+import './assets/style.res.css';
 import './App.css';
 import { createMuiTheme, ThemeProvider, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+import Layout from './component/Layout/Layout'
+import Personal from './component/Doctor/Personal/Personal'
 
 
 function App() {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up('sm'));
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-    </div>
-  );
+    return (
+      <div>
+        <Layout>
+          <Personal />
+        </Layout>
+      </div>
+    );
 }
+
 
 export default App;
